86fc285eb04244f62a11f90c75ffb779b89a3de2
Marjan Ilovski <ilovski5@gmail.com>
2025-01-15 13:11:25 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { Redis } from "ioredis";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const redis = new Redis("redis://default:password@redis:6379/0");
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+app.get("/", async (c) => {
+  console.log(redis.status);
+
+  const res = await redis.ping();
+  console.log(res);
+
+  return c.text("Hello Hono!");
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
